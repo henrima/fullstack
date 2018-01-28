46248fd0e11ff7c452ec27840b9d5f2d3cc35147
Henri Malkki <henri.malkki@perfektio.fi>
2018-01-28 20:24:40 +0200

    1.5

--- week1/osa1/src/index.js
+++ week1/osa1/src/index.js
@@ -54,8 +54,9 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
     {
       nimi: 'Reactin perusteet',
       tehtavia: 10
@@ -69,12 +70,15 @@ const App = () => {
       tehtavia: 14
    }
   ]
+  }
+
+console.log(kurssi.osat)
 
   return (
     <div>
-      <Otsikko kurssi={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa osat={osat} />
+      <Otsikko kurssi={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa osat={kurssi.osat} />
     </div>
   )
 }

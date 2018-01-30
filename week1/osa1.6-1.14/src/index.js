3eeb225cc1b5fd21efcd3e763eee5a5e6b3cb6a6
Henri Malkki <henri.malkki@perfektio.fi>
2018-01-30 17:48:12 +0200

    1.8-1.9

--- week1/osa1.6-1.14/src/index.js
+++ week1/osa1.6-1.14/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = (props) => (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+
+const Statistics = (props) => {
+    const yhteensa = props.hyva + props.neutraali + props.huono
+    const keskiarvo = (props.hyva - props.huono) / yhteensa
+    const positiivisia = (props.hyva)/(props.hyva+props.neutraali+props.huono)*100
+
+    return ( 
+        <div>
+            { props.hyva !== 0 && <Statistic text="hyva" value={props.hyva} /> }
+            { props.neutraali !== 0 && <Statistic text="neutraali" value={props.neutraali} /> }
+            { props.huono !== 0 && <Statistic text="huono" value={props.huono} /> }
+            { isNaN(keskiarvo) === false && <p>keskiarvo {keskiarvo}</p> }
+            { isNaN(positiivisia) === false && <p>positiivisia {positiivisia} %</p> }
+            { yhteensa === 0 && <p> ei yhtään palautetta annettu </p> }
+        </div> 
+    )
+}
+
+const Statistic = (props) => (
+    <p>{props.text} {props.value}</p>
+)
 
 class App extends React.Component {
     constructor(props) {
@@ -12,22 +36,13 @@ class App extends React.Component {
       }
     }
   
-    klikHyva = () => {
-      this.setState({
-        hyva: this.state.hyva + 1,
-      })
-    }
-  
-    klikNeutraali = () => {
-      this.setState({
-        neutraali: this.state.neutraali + 1,
-      })
-    }
  
-    klikHuono = () => {
-        this.setState({
-          huono: this.state.huono + 1,
-        })
+    asetaArvo = (state, value) => {
+        return () => {
+            this.setState({
+                [state]: value,
+            })
+        }
     }    
 
     render() {
@@ -35,15 +50,11 @@ class App extends React.Component {
         <div>
           <div>
             <h1>anna palautetta</h1>
-            <button onClick={this.klikHyva}>hyvä</button>
-            <button onClick={this.klikNeutraali}>neutraali</button>
-            <button onClick={this.klikHuono}>huono</button>
+            <Button onClick={this.asetaArvo("hyva", this.state.hyva + 1)} text="hyvä"/>
+            <Button onClick={this.asetaArvo("neutraali", this.state.neutraali + 1)} text="neutraali"/>
+            <Button onClick={this.asetaArvo("huono", this.state.huono + 1)} text="huono"/>
             <h1>statistiikka</h1>
-            <p>hyvä {this.state.hyva}</p>
-            <p>neutraali {this.state.neutraali}</p>
-            <p>huono {this.state.huono}</p>
-            <p>keskiarvo {(((this.state.hyva * 1)+(this.state.huono * -1)) / (this.state.hyva + this.state.huono))}</p>
-            <p>positiivisia {(this.state.hyva)/(this.state.hyva+this.state.neutraali+this.state.huono)*100} %</p>
+            <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} />
           </div>
         </div>
       )

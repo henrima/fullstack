11d417283bfe00fd9bd1af08b7594546afaed3d7
Henri Malkki <henri.malkki@perfektio.fi>
2018-02-16 16:53:57 +0200

    4.2

--- week4/blogilista/index.js
+++ week4/blogilista/index.js
@@ -4,44 +4,30 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config()
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = process.env.MLAB_URI
-mongoose.connect(mongoUrl)
+
+mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+app.use('/api/blogs', blogsRouter)
+
+
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+server.on('close', () => {
+  mongoose.connection.close()
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = {
+  app, server
+}

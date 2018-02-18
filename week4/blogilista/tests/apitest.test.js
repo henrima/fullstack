9c778e16f66d203f1ab50774dcb80d65ddbac5e3
Henri Malkki <henri.malkki@perfektio.fi>
2018-02-18 17:50:11 +0200

    4.9

--- week4/blogilista/tests/apitest.test.js
+++ week4/blogilista/tests/apitest.test.js
@@ -1,6 +1,67 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
+const Blog = require('../models/blog')
+
+const listWithSomeBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+  ]
+
+beforeAll(async () => {
+    await Blog.remove({})
+
+    const blogObjects = listWithSomeBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+
+    await Promise.all(promiseArray)
+})
 
 test('notes are returned as json', async () => {
   await api
@@ -8,6 +69,31 @@ test('notes are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('a valid blog can be added ', async () => {
+    const newBlog = {
+        _id: "5a422bc61b54a676234d17fd",
+        title: "Peten paluu",
+        author: "Pepe Willberg",
+        url: "http://jesjes.com/ml",
+        likes: 18,
+        __v: 0
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api
+      .get('/api/blogs')
+  
+    const titles = response.body.map(r => r.title)
+  
+    expect(response.body.length).toBe(listWithSomeBlogs.length + 1)
+    expect(titles).toContainEqual("Peten paluu")
+  })
 
 afterAll(() => {
   server.close()
